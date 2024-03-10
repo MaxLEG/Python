618f8e581b58c8c146801cd5f18ced682d08cc24
MaxLEG <soldier322@gmail.com>
2024-03-10 10:35:49 +0100

    upgrade visualisation

--- Work with API/python_repos_best.py
+++ Work with API/python_repos_best.py
@@ -22,7 +22,16 @@ for repo_dict in repo_dicts:
 
 """Visualisation building"""
 my_style = LS("#333366", base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = "Most-Starred PYTHON Projects on GitHub"
 chart.x_labels = names
 chart.add(" ", stars)
@@ -49,6 +58,3 @@ for key in sorted(repo_dict.keys()):
 
 """Processing the results"""
 """ print(response_dict.keys()) """
-
-
-"""PAGE 369"""
