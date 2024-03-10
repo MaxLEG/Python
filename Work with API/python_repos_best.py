894a865f5c0c148468cba3fcc554d73e8bb87c2c
MaxLEG <soldier322@gmail.com>
2024-03-10 10:23:33 +0100

    add visualisation

--- Work with API/python_repos_best.py
+++ Work with API/python_repos_best.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 """creating API request and saving the response"""
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
@@ -13,8 +15,22 @@ print("Total repositories: ", response_dict["total_count"])
 repo_dicts = response_dict["items"]
 print("Repositories returned: ", len(repo_dicts))
 
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+"""Visualisation building"""
+my_style = LS("#333366", base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = "Most-Starred PYTHON Projects on GitHub"
+chart.x_labels = names
+chart.add(" ", stars)
+chart.render_to_file("Work with API/python_repos.svg")
+
+
 """Analysis of 1st repository"""
-repo_dict = repo_dicts[0]
+"""repo_dict = repo_dicts[0]
 
 
 print("\nSelected information about each repository:")
@@ -25,14 +41,14 @@ for repo_dict in repo_dicts:
     print("Repository:", repo_dict["html_url"])
     print("Created:", repo_dict["created_at"])
     print("Updated:", repo_dict["updated_at"])
-    print("Description:", repo_dict["description"])
+    print("Description:", repo_dict["description"]) """
 
 """ print("\nKeys: ", len(repo_dict))
 for key in sorted(repo_dict.keys()):
     print(key) """
 
 """Processing the results"""
-print(response_dict.keys())
+""" print(response_dict.keys()) """
 
 
 """PAGE 369"""
